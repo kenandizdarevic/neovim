26cf5f8ee426b845b4c4267906ac155f2e9e385b
Kenan Dizdarevic <kenan.dizdareviic@gmail.com>
2024-04-18 14:37:31 +0200

    feat: add specific options

---
+++ lua/keno/options.lua
@@ -0,0 +1,14 @@
+-- :help options
+vim.opt.backup = false 			-- creates a backup file
+vim.opt.clipboard = "unnamedplus"	-- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2			-- more space in the CLI
+vim.opt.fileencoding = "utf-8"		-- set fileencoding to utf-8
+vim.opt.expandtab = true		-- converts tabs to spaces
+vim.opt.shiftwidth = 4          -- use 4 space  -- use 4 spaces
+vim.opt.tabstop = 4			    -- 4 spaces for tab
+--vim.opt.cursorline = true 		-- highlight current line
+vim.opt.ignorecase = true       -- ignore case in search patterns
+vim.opt.numberwidth = 4         -- set number width, default is 4
+vim.opt.wrap = false			-- displays lines as one long
+vim.opt.number = true 			-- set numbered lines
+vim.opt.showmode = false		-- remove --INSERT--
