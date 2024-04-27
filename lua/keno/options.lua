c9e6350bb641b9f287cbd099c3602fd5c92df810
Kenan Dizdarevic <kenan.dizdareviic@gmail.com>
2024-04-27 23:36:54 +0200

    feat: new options added

--- lua/keno/options.lua
+++ lua/keno/options.lua
@@ -1,14 +1,46 @@
--- :help options
-vim.opt.backup = false 			-- creates a backup file
-vim.opt.clipboard = "unnamedplus"	-- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2			-- more space in the CLI
-vim.opt.fileencoding = "utf-8"		-- set fileencoding to utf-8
-vim.opt.expandtab = true		-- converts tabs to spaces
-vim.opt.shiftwidth = 4          -- use 4 space  -- use 4 spaces
-vim.opt.tabstop = 4			    -- 4 spaces for tab
---vim.opt.cursorline = true 		-- highlight current line
-vim.opt.ignorecase = true       -- ignore case in search patterns
-vim.opt.numberwidth = 4         -- set number width, default is 4
-vim.opt.wrap = false			-- displays lines as one long
-vim.opt.number = true 			-- set numbered lines
-vim.opt.showmode = false		-- remove --INSERT--
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+--termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+  tabstop = 4,                             -- insert 2 spaces for a tab
+  cursorline = false,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                            -- display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
